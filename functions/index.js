e50f2b7313323ba768e00b793c7ce1ac6fc2dece
김동욱 <igerapex@gmail.com>
2020-11-08 15:51:56 +0900

    Add Naver local place search cloud function

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 const dbService = admin.firestore();
+const Axios = require("axios");
 
 exports.createUser = functions.auth.user().onCreate((user) => {
   return dbService.collection("users").doc(user.uid).set({
@@ -15,3 +16,20 @@ exports.deleteUser = functions.auth.user().onDelete((user) => {
   const doc = dbService.collection("users").doc(user.uid);
   return doc.delete();
 });
+
+/* 네이버 지역검색 */
+exports.searchNaverPlace = functions
+  .region("asia-northeast1")
+  .https.onCall(async (data, context) => {
+    const result = await Axios.get(
+      "https://openapi.naver.com/v1/search/local.json",
+      {
+        params: { query: data, display: 5 },
+        headers: {
+          "X-Naver-Client-Id": functions.config().naverplace.id,
+          "X-Naver-Client-Secret": functions.config().naverplace.secret,
+        },
+      }
+    );
+    return result.data;
+  });
