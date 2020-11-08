2272c7f9d494d9d50c89c855b9de62f1ed53a76b
김동욱 <igerapex@gmail.com>
2020-11-09 01:03:05 +0900

    Set react-axe for accessibility

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import axe from "react-axe"
 import App from "./App";
+
+if (process.env.NODE_ENV !== "production") {
+  axe(React, ReactDOM, 1000)
+}
 
 ReactDOM.render(
   <React.StrictMode>
